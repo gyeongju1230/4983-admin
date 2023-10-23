6ef9aad8dfd3040020c0fd4315083fa519d9c864
wan2daaa <wan2daaa@gmail.com>
2023-10-23 21:53:35 +0900

    feat: nextjs 세팅 및 globalStyle 지정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import GlobalStyles from "@/styles/globalStyles";
+import { TopBar } from "@/components/common/top-bar/TopBar";
+import { SideTab } from "@/components/common/side-tab/SideTab";
+import { MainContent } from "@/components/common/main-content/MainContent";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <TopBar />
+      <div style={{ display: "flex", gap: "1.1rem" }}>
+        <SideTab />
+        <MainContent>
+          <Component {...pageProps} />
+        </MainContent>
+      </div>
+    </>
+  );
 }
